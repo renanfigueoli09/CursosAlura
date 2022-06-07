5555cc6615758833ecfac1865f0869cd39cb968d
renanfigueoli <renanfigueoli@outlook.com>
2022-06-07 15:00:20 -0300

    criando o curso LayotsResponsivos

--- JS_priemeiro passo com a liguagem/2-trabalhandoComVariaveis.js
+++
@@ -1,9 +0,0 @@
-console.log("Trabalhando com variáveis");
-//JS é case sentive: letras maiusculas e minusculas tem diferença
-const idade=29;
-const nome="renan";
-let ano=2020;
-
-console.log(idade);
-console.log(nome);
-console.log(ano);
